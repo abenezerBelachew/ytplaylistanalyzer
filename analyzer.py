6e98a24622cea5214bfffa5c65c41ac67f51233d
AB <belachew@ualberta.ca>
2021-08-27 14:39:41 -0600

    .

--- analyzer.py
+++ analyzer.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from googleapiclient.discovery import build
 
 
-api_key = os.environ.get('YTAPIKEY')
+api_key = ''
 youtube = build("youtube", "v3", developerKey=api_key)
 
 
